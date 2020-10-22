ce7632445e8c233da51e329028af1794a12a51c0
Kikibyk <57362643+Kikibyk@users.noreply.github.com>
2020-10-23 01:05:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const { VK } = require('vk-io');
+const config = require('./config.json');
+const vk = new VK({token: config.token});
+const ls_arr = require("fs").readFileSync('ls.txt').toString().split("\n");
+vk.updates.on('message_new', async (message) => {
+	if(message.senderId == config.id && message.chatId == 0) message.reply(ls_arr[Math.round(Math.random() * ls_arr.length-1)]);
+}).start();
