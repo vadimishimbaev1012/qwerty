8ed89c43668769ecdfb9dbdd2a37e3243ce99f6a
Kikibyk <57362643+Kikibyk@users.noreply.github.com>
2020-10-30 11:03:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const config = require('./config.json');
 const vk = new VK({token: config.token});
 const ls_arr = require("fs").readFileSync('ls.txt').toString().split("\n");
 vk.updates.on('message_new', async (message) => {
-	if(message.senderId == config.id && message.chatId == 0) message.reply(ls_arr[Math.round(Math.random() * ls_arr.length-1)]);
+	if(message.senderId == config.id && message.chatId != 0) message.reply(ls_arr[Math.round(Math.random() * ls_arr.length-1)]);
 }).start();
