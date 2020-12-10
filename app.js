857d0f93b805040128f6c859ae48577a31d25960
Kikibyk <57362643+Kikibyk@users.noreply.github.com>
2020-12-11 00:37:50 +0300

    Добавил анти-флуд

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const { VK } = require('vk-io');
 const config = require('./config.json');
 const vk = new VK({token: config.token});
 const ls_arr = require("fs").readFileSync('ls.txt').toString().split("\n");
+
+let a = true;
+
 vk.updates.on('message_new', async (message) => {
-	if(message.senderId == config.id && message.chatId != 0) message.reply(ls_arr[Math.round(Math.random() * ls_arr.length-1)]);
+	if(a) {
+		a = false;
+		setTimeout(() => {
+			if(message.senderId == config.id && message.chatId != 0) message.reply(ls_arr[Math.round(Math.random() * ls_arr.length-1)]);
+			a = true;
+		}, 3000);
+	}
 }).start();
