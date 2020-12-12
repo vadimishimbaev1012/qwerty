692f9ca84ec040619d0698c4162d1684d1ffcf8d
Kikibyk <57362643+Kikibyk@users.noreply.github.com>
2020-12-13 00:27:07 +0300

    Добавил статус печатания сообщения

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const ls_arr = require("fs").readFileSync('ls.txt').toString().split("\n");
 let a = true;
 
 vk.updates.on('message_new', async (message) => {
-	if(a) {
+	if(message.senderId == config.id && message.chatId != 0 && a) {
 		a = false;
+		vk.api.messages.setActivity({peer_id: message.peerId, type: "typing"});
 		setTimeout(() => {
-			if(message.senderId == config.id && message.chatId != 0) message.reply(ls_arr[Math.round(Math.random() * ls_arr.length-1)]);
 			a = true;
+			message.reply(ls_arr[Math.round(Math.random() * ls_arr.length-1)]);
 		}, 3000);
 	}
 }).start();
