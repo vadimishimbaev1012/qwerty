b293053c970b59909d7dec1cb0229b513528006a
Kikibyk <57362643+Kikibyk@users.noreply.github.com>
2021-01-28 06:26:50 +0300

    Теперь надо указывать ID жертвы как аргумент
    Чтобы указать ID, нужно вписать его после "node app.js"
    Пример: node app.js 1583536

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const ls_arr = require("fs").readFileSync('ls.txt').toString().split("\n");
 
 let a = true;
 
+if(!process.argv[2]) return console.error(`Вы не указали ID!\nnode app.js <id>`);
+
 vk.updates.on('message_new', async (message) => {
 	client.api.messages.getByConversationMessageId({peer_id: message.peerId, conversation_message_ids: message.conversationMessageId}).then((result) => {
-		if(result.from_id == config.id && a) {
+		if(result.from_id == process.argv[2] && a) {
 			a = false;
 			vk.api.messages.setActivity({peer_id: message.peerId, type: "typing"});
 			setTimeout(() => {
