67ed8e7b89759e7355ba3f653a81cd07e51b992c
Илья <57362643+Kikibyk@users.noreply.github.com>
2021-10-12 00:59:08 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-const { VK } = require('vk-io');
-const config = require('./config.json');
-const vk = new VK({token: config.token});
-const ls_arr = require("fs").readFileSync('ls.txt').toString().split("\n");
+if(!process.argv[2]) return console.error(`\x1b[31m> \x1b[0mНеобходимо указать id/domain/link пользователя, на кого будет работать скрипт.\n\x1b[31m> \x1b[0mИспользование: node app.js <id/domain/link>`);
+const { api, updates } = new (require('vk-io')).VK(require('./config.json'));
 
-let a = true;
+const templates = require("fs").readFileSync('templates.txt').toString().split("\n");
 
-if(!process.argv[2]) return console.error(`Вы не указали ID!\nnode app.js <id>`);
+let typing = false, target;
+updates.on("message_new", async ({id, peerId, send, conversationMessageId}) => {
+    if(typing) return; // Если его текст уже в обработке, новые сообщения не принимаются (анти-флуд)
 
-vk.updates.on('message_new', async (message) => {
-	vk.api.messages.getByConversationMessageId({peer_id: message.peerId, conversation_message_ids: message.conversationMessageId}).then((result) => {
-		if(result.items[0].from_id == process.argv[2] && a) {
-			a = false;
-			vk.api.messages.setActivity({peer_id: result.items[0].peer_id, type: "typing"});
-			setTimeout(() => {
-				a = true;
-				message.reply(ls_arr[Math.round(Math.random() * ls_arr.length-1)]);
-			}, 3000);
-		}	
-	});
-}).start();
+    const [message] = (await api.messages.getByConversationMessageId({conversation_message_ids: conversationMessageId, peer_id: peerId})).items; // Запрос сообщения, чтобы бот работал в ЛС (вк не даёт автора сообщения в ЛС)
+    if(message.from_id !== target) return; else typing = true; // Проверка на то, является ли автор сообщения нашей жертвой
+
+    await api.messages.markAsRead({peer_id: peerId, start_message_id: id}); // Читаем сообщение
+    await sleep(2500); // Делаем вид что мы читаем его сообщение
+
+    const template = templates[id%templates.length]; // Получаем строку из шаблонов
+    await api.messages.setActivity({peer_id: peerId, type: "typing"}); // Включаем статус печати
+    await sleep(Math.random()*2000+template.length*30); // Делаем вид что печатаем текст
+
+    await api.messages.send({message: template, peer_id: peerId, random_id: 0, reply_to: peerId > 2e9 ? id : undefined}); // Отправляем шаблон
+    typing = false; // Включаем приём новых сообщений от него
+});
+
+updates.start().then(async () => {
+    target = (await require('vk-io').resolveResource({api, resource: process.argv[2]})).id;
+    const [{first_name, last_name}] = await api.users.get({user_ids: target});
+
+    console.log(`\x1b[32m> \x1b[0mБот был успешно запущен`);
+    console.log(`\x1b[32m> \x1b[0mЦель: ${first_name} ${last_name}`);
+});
+async function sleep(ms) { return await new Promise((resolve) => setTimeout(resolve, ms)) }
